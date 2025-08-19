c673ff71427d6dfdb9dc0437d74633d1e9c71f66
nalodagreat <nalothegreat0@gmail.com>
2025-08-19 18:35:03 +0200

    adding proprities to the manager >> -getters&setters >> -calculate salry+adding bonus base on managed thecnicians >> -possibility of adding new thecnician in the Manager list

--- HR Management System/Technician.h
+++ HR Management System/Technician.h
@@ -3,19 +3,25 @@
 class Technician : public Employee
 {
 	string specialization;
+	string cfManagedByManager; // this is the tax code of the manager who manages this technician
 public:
-	Technician(string role, string cf, string name, string surname, Date dateOfStartWorking, string specialization)
+	Technician(string role, string cf, string name, string surname, Date dateOfStartWorking, string specialization, string cfManagedByManager)
 		: Employee(role, cf, name, surname, dateOfStartWorking)
 	{
 		this->specialization = specialization;
+		this->cfManagedByManager = cfManagedByManager;
 	}
-	string getSpecialization() 
+	string getSpecialization()
 	{
 		return specialization;
 	}
 	void setSpecialization(string specialization)
 	{
 		this->specialization = specialization;
+	}
+	string getCfManagedByManager()
+	{
+		return cfManagedByManager;
 	}
 	int calculateTechnicianSalary()
 	{
