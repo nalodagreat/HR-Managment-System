ccda8688c8ebbd6ed85902bf8924a3c85b3ec60e
nalodagreat <nalothegreat0@gmail.com>
2025-08-19 18:26:56 +0200

    adding managedby manager data memeber

--- HR Management System/Manager.h
+++ HR Management System/Manager.h
@@ -1,5 +1,50 @@
 #pragma once
-class Manager
+#include "Employee.h"
+#include <string>
+#include <iostream>
+#include "Date.h"
+#include "Technician.h"
+#include <vector>
+using namespace std;
+class Manager : public Employee
 {
+	string responsibility;
+	string cfMangedByExecutives; // this is the tax code of the executive who manages this manager
+	vector<Technician> vTechnicians; //list of technicians who report to this manager
+public:
+	Manager(string role, string cf, string name, string surname, Date dateOfStartWorking, string responsibility,string cfMangedByExecutives)
+		: Employee(role, cf, name, surname, dateOfStartWorking)
+	{
+		this->responsibility = responsibility;
+		this->cfMangedByExecutives = cfMangedByExecutives;
+	}
+	string getResponsibility() {
+		return responsibility;
+	}
+	void setResponsibility(string responsibility) {
+		this->responsibility = responsibility;
+	}
+	void addTechnician(Technician technician) {
+		vTechnicians.push_back(technician);
+	}
+	vector<Technician> getTechnicians() {
+		return vTechnicians;
+	}
+	
+    void addTechnicianToManagerList(Technician technician )
+	{
+		vTechnicians.push_back(technician);
+	}
+	double calculateSalary() {
+		double salary = 2000.0; // base salary for manager
+		for (Technician tech : vTechnicians) {
+			if (tech.getCfManagedByManager() == this->getCf()) {
+				salary *= 0.1;
+			}
+		}
+		return salary;
+	}
+
+
 };
 
