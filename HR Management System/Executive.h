40e7783da37e74bbfa661073c3602c91ebb1969e
nalodagreat <nalothegreat0@gmail.com>
2025-08-20 22:29:53 +0200

    feat:adding Exective

---
+++ HR Management System/Executive.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Employee.h"
+#include <string>
+#include <iostream>
+#include "Date.h"
+#include <vector>
+using namespace std;
+class Executive : public Employee
+{
+	string divisionName; // name of the division managed by the executive
+	vector<Employee> vEmployees; // list of employees in the company
+public:
+	Executive(string role, string cf, string name, string surname, Date dateOfStartWorking, string divisionName)
+		: Employee(role, cf, name, surname, dateOfStartWorking)
+	{
+		this->divisionName = divisionName;
+	}
+	string getDivisionName() {
+		return divisionName;
+	}
+	void setDivisionName(string divisionName) {
+		this->divisionName = divisionName;
+	}
+	void addEmployee(Employee employee) {
+		vEmployees.push_back(employee);
+	}
+	vector<Employee> getEmployees() {
+		return vEmployees;
+	}
+	
+};
+
