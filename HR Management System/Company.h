c1f10e645d9886d8ad94811112ee288353156fc1
nalodagreat <nalothegreat0@gmail.com>
2025-08-20 22:59:50 +0200

    feat:loadEmployeesFromFile

--- HR Management System/Company.h
+++ HR Management System/Company.h
@@ -16,6 +16,45 @@ class Company
 	private:
 	string companyName; // name of the company
 	vector<Employee> vEmployees; // list of employees in the company
+	vector<Employee> loadEmployeesFromFile(string filename = "employees.txt") {
+		vector<Employee> employees;
+		ifstream file(filename);
+		if (!file.is_open()) {
+			cout << "Error opening file: " << filename << endl;
+			return employees; // return empty vector if file cannot be opened
+		}
+		string line;
+		while (getline(file, line)) 
+		{
+			vector<string> data = clsString::Split(line,";");
+			if (data.size() < 5) 
+				continue;
+			string role = data[0];
+			string cf = data[1];
+			string name = data[2];
+			string surname = data[3];
+			Date dateOfStartWorking= Date::convertStringToDat(data[4]);
+			if (role == "Technician") {
+				string specialization = data[5];
+				string cfManagedByManager = data[6];
+				Technician technician(role, cf, name, surname, dateOfStartWorking, specialization, cfManagedByManager);
+				employees.push_back(technician);
+			}
+			else if (role == "Manager") {
+				string responsibility = data[5];
+				string cfManagedByExecutives = data[6];
+				Manager manager(role, cf, name, surname, dateOfStartWorking, responsibility, cfManagedByExecutives);
+				employees.push_back(manager);
+			}
+			else if (role == "Executive") {
+				string divisionName = data[5];
+				Executive executive(role, cf, name, surname, dateOfStartWorking, divisionName);
+				employees.push_back(executive);
+			}
+		}
+		file.close();
+		return employees;
+	}
 	
 public:
 	Company(string companyName) : companyName(companyName) {}
@@ -40,7 +79,7 @@ public:
 	}
 	bool employeeExists(string cf) {
 		for (Employee emp : vEmployees) {
-			if (emp.getCf() ==	cf) {
+			if (emp.getCf() == cf) {
 				return true; // Employee already exists
 			}
 		}
