5b53fff36c51bbc1113e1dd3e7ae23a28562a0bd
nalodagreat <nalothegreat0@gmail.com>
2025-09-01 23:52:49 +0200

    Refactor: Introduce Employee abstract base class

--- HR Management System/Employee.h
+++ HR Management System/Employee.h
@@ -1,74 +1,47 @@
 #pragma once
 #include <string>
-#include "Date.h"
+
+// Per evitare di scrivere sempre "std::" prima di string, cout, ecc.
 using namespace std;
-class Employee
-{
-	string role;
-	string cf;
-	string name;
-	string surname;
-	Date dateOfStartWorking;
+
+/*
+ * Classe Base Astratta Employee
+ * Rappresenta un dipendente generico.
+ * "Astratta" significa che non possiamo creare un oggetto di tipo "Employee",
+ * ma la usiamo come "stampo" per le altre classi (Technician, Manager, ...).
+*/
+class Employee {
+protected:
+    // Dati protetti: accessibili da questa classe e dalle classi figlie.
+    string taxCode;
+    string firstName;
+    string lastName;
+    string hireDate;
+
 public:
-	Employee(string role, string cf, string name, string surname, Date dateOfStartWorking)
-	{
-		this->role = role;
-		this->cf = cf;
-		this->name = name;
-		this->surname = surname;
-		this->dateOfStartWorking = dateOfStartWorking;
-	}
-	string getRole()
-	{
-		return role;
-	}
-	string getCf()
-	{
-		return cf;
-	}
-	string getName()
-	{
-		return name;
-	}
-	string getSurname()
-	{
-		return surname;
-	}
-	Date getDateOfStartWorking()
-	{
-		return dateOfStartWorking;
-	}
-	void setRole(string role)
-	{
-		this->role = role;
-	}
-	void setCf(string cf)
-	{
-		this->cf = cf;
-	}
-	void setName(string name)
-	{
-		this->name = name;
-	}
-	void setSurname(string surname)
-	{
-		this->surname = surname;
-	}
-	void setDateOfStartWorking(Date dateOfStartWorking)
-	{
-		this->dateOfStartWorking = dateOfStartWorking;
-	}
-	virtual double calculateSalary()
-	{
-		// Base salary calculation logic can be overridden in derived classes
-		return 0.0;
-	}
-	virtual string getSpecialization() { return ""; }
-	virtual string getCfManagedByManager() { return ""; }
-	virtual string getCfManagedByExecutives() { return ""; }
-	virtual string getResponsibility() { return ""; }
-	virtual string getDivisionName() { return ""; }
+    // Costruttore: serve a creare un oggetto Employee con i suoi dati base.
+    Employee(const string& tc, const string& fn, const string& ln, const string& hd) {
+        taxCode = tc;
+        firstName = fn;
+        lastName = ln;
+        hireDate = hd;
+    }
 
+    // Distruttore virtuale: fondamentale nei design con ereditarieta.
+    // Garantisce che, quando eliminiamo un puntatore a Employee, venga chiamato
+    // anche il distruttore della classe figlia (es. Technician).
+    virtual ~Employee() {}
 
+    /*
+     * Metodo virtuale puro: questo "= 0" rende la classe astratta.
+     * Obbliga ogni classe figlia (Technician, Manager, ...) a scrivere la propria
+     * versione di "calculateSalary". Non c'e un'implementazione di default qui.
+    */
+    virtual double calculateSalary() = 0;
+
+    // Metodi "getter": servono a leggere i valori delle variabili protette dall'esterno.
+    string getTaxCode() const { return taxCode; }
+    string getFirstName() const { return firstName; }
+    string getLastName() const { return lastName; }
+    string getHireDate() const { return hireDate; }
 };
-
